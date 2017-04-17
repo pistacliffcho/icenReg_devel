689ec73654a916f08fed6dc4dbe0162f437814d0
Cliff Anderson-Bergman <cliff@Cliffords-MacBook-Air.local>
2017-04-16 22:20:40 -0700

    sent to r-winbuilder devel

--- Code/icenReg/src/registeringRoutines.c
+++ Code/icenReg/src/registeringRoutines.c
@@ -1,23 +1,20 @@
-/*
-
 #include <R.h>
 #include <Rinternals.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
- */
- 
 /* FIXME: 
    Check these declarations against the C/Fortran source code.
 */
 
 /* .Call calls */
-/*
 extern SEXP dGeneralGamma(SEXP, SEXP, SEXP, SEXP);
 extern SEXP EMICM(SEXP, SEXP, SEXP);
 extern SEXP fastNumericInsert(SEXP, SEXP, SEXP);
 extern SEXP findMI(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP ic_sp_ch(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP icenReg_computeConditional_p(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP icenReg_computeConditional_q(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP icenReg_ic_parList(SEXP);
 extern SEXP icenReg_R_ic_bayes(SEXP, SEXP, SEXP);
 extern SEXP optCliq(SEXP, SEXP, SEXP, SEXP, SEXP);
@@ -26,17 +23,19 @@ extern SEXP qGeneralGamma(SEXP, SEXP, SEXP, SEXP);
 extern SEXP s_regTrans(SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"dGeneralGamma",      (DL_FUNC) &dGeneralGamma,       4},
-    {"EMICM",              (DL_FUNC) &EMICM,               3},
-    {"fastNumericInsert",  (DL_FUNC) &fastNumericInsert,   3},
-    {"findMI",             (DL_FUNC) &findMI,              5},
-    {"ic_sp_ch",           (DL_FUNC) &ic_sp_ch,           11},
-    {"icenReg_ic_parList", (DL_FUNC) &icenReg_ic_parList,  1},
-    {"icenReg_R_ic_bayes", (DL_FUNC) &icenReg_R_ic_bayes,  3},
-    {"optCliq",            (DL_FUNC) &optCliq,             5},
-    {"q_regTrans",         (DL_FUNC) &q_regTrans,          5},
-    {"qGeneralGamma",      (DL_FUNC) &qGeneralGamma,       4},
-    {"s_regTrans",         (DL_FUNC) &s_regTrans,          5},
+    {"dGeneralGamma",                (DL_FUNC) &dGeneralGamma,                 4},
+    {"EMICM",                        (DL_FUNC) &EMICM,                         3},
+    {"fastNumericInsert",            (DL_FUNC) &fastNumericInsert,             3},
+    {"findMI",                       (DL_FUNC) &findMI,                        5},
+    {"ic_sp_ch",                     (DL_FUNC) &ic_sp_ch,                     11},
+    {"icenReg_computeConditional_p", (DL_FUNC) &icenReg_computeConditional_p,  5},
+    {"icenReg_computeConditional_q", (DL_FUNC) &icenReg_computeConditional_q,  5},
+    {"icenReg_ic_parList",           (DL_FUNC) &icenReg_ic_parList,            1},
+    {"icenReg_R_ic_bayes",           (DL_FUNC) &icenReg_R_ic_bayes,            3},
+    {"optCliq",                      (DL_FUNC) &optCliq,                       5},
+    {"q_regTrans",                   (DL_FUNC) &q_regTrans,                    5},
+    {"qGeneralGamma",                (DL_FUNC) &qGeneralGamma,                 4},
+    {"s_regTrans",                   (DL_FUNC) &s_regTrans,                    5},
     {NULL, NULL, 0}
 };
 
@@ -45,5 +44,3 @@ void R_init_icenReg(DllInfo *dll)
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
-
-*/
